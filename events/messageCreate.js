52556d8ecf7a754b121e53adc65494b7dce009a7
dirtbirb <dirtbirb@gmail.com>
2022-07-07 00:56:16 -0700

    readability

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,31 +1,37 @@
 const sounds = require('../kitten-sounds.js')
 
-module.exports = (client, int) => {
+module.exports = (client, interaction) => {
   // First and foremost, acknowledge the funny number
-  if (int.content.includes('69')) {
-    int.react('<:NICE:466837748263682059>')
+  if (interaction.content.includes('69')) {
+    interaction.react('<:NICE:466837748263682059>')
   }
 
   // Ignore bots (including self)
-  if (int.author.bot) return
+  if (interaction.author.bot) return
+
+  // // Handle DMs
+  // if (int.channel.type === 'DM') {
+  //   console.log(int.content)
+  //   int.reply('Hello!')
+  // }
 
   // Parse arguments and check for prefix
-  const args = int.content.split(' ')
+  const args = interaction.content.split(' ')
   if (args.shift() !== client.config.prefix) return
 
   // Check that command exists
   const command = client.commands.get(args.shift())
   if (!command) {
-    return int.reply({
+    return interaction.reply({
       content: sounds.confused(),
       ephemeral: true
     })
   }
 
   // Trigger command directly
-  int.args = args
-  int.silent = false
-  int.user = int.author
-  console.log(`Running message command: ${int.commandName}`)
-  command.run(client, int)
+  interaction.args = args
+  interaction.silent = false
+  interaction.user = interaction.author
+  console.log(`Running message command: ${interaction.commandName}`)
+  command.run(client, interaction)
 }
