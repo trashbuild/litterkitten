4783b74571ea640f0c798200dfa3635d22716043
dirtbirb <dirtbirb@gmail.com>
2022-07-09 18:40:59 -0700

    improve wednesday logic

--- commands/wednesday.js
+++ commands/wednesday.js
@@ -7,16 +7,13 @@ module.exports = {
   run: async (client, interaction) => {
     // Load Wednesday playlist from config file
     interaction.args = [client.config.wednesday]
-    client.commands.get('play').run(client, interaction)
-      // Wait 5 seconds for queue creation
+    // Play and trigger shuffle after 5 seconds
+    await client.commands.get('play').run(client, interaction)
       .then(setTimeout(() => {
-        // Shuffle queue
         interaction.silent = true
         client.commands.get('shuffle').run(client, interaction)
-        // Set to conversational volume
-        interaction.args = [25]
-        client.commands.get('volume').run(client, interaction)
-      }, 5000)
-      )
+      }, 5000))
+    // Set to conversational volume
+    client.player.getQueue(interaction.guild.id).setVolume(25)
   }
 }
