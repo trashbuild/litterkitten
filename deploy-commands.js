1295080d69fc7a5f571a198a8c05408a0f855d0e
Dave M <dave@swiftcoat.com>
2022-10-14 11:12:36 -0300

    fix duplicate commands (register all as global)

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,20 +1,41 @@
 const fs = require('node:fs')
 const path = require('node:path')
 const { REST, Routes } = require('discord.js')
-const { clientId, guildId, token } = require('./config.json')
+const {
+  clientId,
+  // guildId,
+  token
+} = require('./config.json')
 
+// Load commands to register
 const commands = []
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
-
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file)
   const command = require(filePath)
   commands.push(command.data.toJSON())
 }
 
+// Communicate with Discord
 const rest = new REST({ version: '10' }).setToken(token)
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// // Delete guild-based commands
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+//   .then(() => console.log('Successfully deleted all guild commands.'))
+//   .catch(console.error)
+
+// // Delete global commands
+// rest.put(Routes.applicationCommands(clientId), { body: [] })
+//   .then(() => console.log('Successfully deleted all application commands.'))
+//   .catch(console.error)
+
+// // Register as guild commands
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+//   .then((data) => console.log(`Successfully registered ${data.length} guild commands.`))
+//   .catch(console.error)
+
+// Register as global commands
+rest.put(Routes.applicationCommands(clientId), { body: commands })
   .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
   .catch(console.error)
