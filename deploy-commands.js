b781bb29ae5f68252c060a679a6d0a73da54f12d
dirtbirb <djmphysics@gmail.com>
2024-05-22 12:42:09 -0700

    register guild-specific commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,10 @@
+const config = require('./config.json')
 const fs = require('node:fs')
 const path = require('node:path')
 const {
   REST,
   Routes
 } = require('discord.js')
-const {
-  clientId,
-  token
-} = require('./config.json')
 
 // Load commands to register
 const commands = []
@@ -20,24 +17,28 @@ for (const file of commandFiles) {
 }
 
 // Communicate with Discord
-const rest = new REST({ version: '10' }).setToken(token)
+const rest = new REST({ version: '10' }).setToken(config.token)
 
-// // Delete guild-based commands
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-//   .then(() => console.log('Successfully deleted all guild commands.'))
-//   .catch(console.error)
+// Delete guild-based commands
+const clientId = config.clientId
+const superGuildId = config.superGuildId
+rest.put(Routes.applicationGuildCommands(clientId, superGuildId), { body: [] })
+  .then(() => console.log('Successfully deleted all guild commands.'))
+  .catch(console.error)
 
-// // Delete global commands
-// rest.put(Routes.applicationCommands(clientId), { body: [] })
-//   .then(() => console.log('Successfully deleted all application commands.'))
-//   .catch(console.error)
+// Register guild commands
+rest.put(Routes.applicationGuildCommands(clientId, superGuildId), { body: commands })
+  .then((data) => console.log(`Successfully registered ${data.length} guild commands.`))
+  .catch(console.error)
 
-// // Register as guild commands
-// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-//   .then((data) => console.log(`Successfully registered ${data.length} guild commands.`))
-//   .catch(console.error)
+// Delete global commands
+rest.put(Routes.applicationCommands(clientId), { body: [] })
+  .then(() => console.log('Successfully deleted all application commands.'))
+  .catch(console.error)
 
-// Register as global commands
-rest.put(Routes.applicationCommands(clientId), { body: commands })
+// Register global commands
+rest.put(Routes.applicationCommands(clientId), {
+  body: commands.filter(cmd => !config.superCommands.includes(cmd.name))
+})
   .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
   .catch(console.error)
